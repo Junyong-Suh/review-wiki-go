d19e676c7ed5d0a1f7e1c4365b97a3a55e5f756e
Junyong Suh <jsuh@linecorp.com>
2019-05-26 19:33:10 +0900

    Removed graceful shutdown

--- review-wiki-go.go
+++ review-wiki-go.go
@@ -1,69 +1,17 @@
 package main
 
 import (
-    "fmt"
-    "flag"
     "log"
-    "time"
-    "os"
-    "os/signal"
-    "context"
     "net/http"
 
     h "github.com/Junyong-Suh/review-wiki-go/handlers"
-
-    "github.com/gorilla/mux"
 )
 
 func main() {
-    var wait time.Duration
-    flag.DurationVar(&wait, "graceful-timeout", time.Second * 15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
-    flag.Parse()
-
-    r := mux.NewRouter()
-    r.HandleFunc("/", h.RootHandler)
-    r.HandleFunc("/view/", h.ViewHandler)
-    r.HandleFunc("/edit/", h.EditHandler)
-    r.HandleFunc("/save/", h.SaveHandler)
-
-    port := "8080"
-    srv := &http.Server{
-        Addr:         fmt.Sprintf("0.0.0.0:%s", port),
-        // Good practice to set timeouts to avoid Slowloris attacks.
-        WriteTimeout: time.Second * 15,
-        ReadTimeout:  time.Second * 15,
-        IdleTimeout:  time.Second * 60,
-        Handler: r, // Pass our instance of gorilla/mux in.
-    }
-
-    // Run our server in a goroutine so that it doesn't block.
-    go func() {
-        // Bind to a port and pass our router in
-        log.Print(fmt.Sprintf("review-wiki-go is running on port %s", port))
-        if err := srv.ListenAndServe(); err != nil {
-            log.Println(err)
-        }
-    }()
-
-    c := make(chan os.Signal, 1)
-    // We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
-    // SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
-    signal.Notify(c, os.Interrupt)
-
-    // Block until we receive our signal.
-    <-c
-
-    // Create a deadline to wait for.
-    ctx, cancel := context.WithTimeout(context.Background(), wait)
-    defer cancel()
-
-    // Doesn't block if no connections, but will otherwise wait
-    // until the timeout deadline.
-    srv.Shutdown(ctx)
-
-    // Optionally, you could run srv.Shutdown in a goroutine and block on
-    // <-ctx.Done() if your application should wait for other services
-    // to finalize based on context cancellation.
-    log.Println(fmt.Sprintf("Shutting down review-wiki-go on port %s", port))
-    os.Exit(0)
+    http.HandleFunc("/", h.RootHandler)
+    http.HandleFunc("/view/", h.ViewHandler)
+    http.HandleFunc("/edit/", h.EditHandler)
+    http.HandleFunc("/save/", h.SaveHandler)
+    log.Println("review-wiki-go is running on port 8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
